c7a5e49165991f8d8564cbe4c7335ab9f89f6de6
lyszt <kalliddel@gmail.com>
2025-04-08 17:33:10 -0300

    sup

--- Exercises/schematic2.vhdl
+++ Exercises/schematic2.vhdl
@@ -1,16 +1,17 @@
-LIBRARY IEEE;
-USE ieeee.std_logic_1164.all/
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
 
 ENTITY schematic2 IS 
     PORT (
         a: IN STD_LOGIC;
         b: IN STD_LOGIC;
         c: IN STD_LOGIC;
-        d, e: IN STD_LOGIC;
+        d, e: OUT STD_LOGIC
     );
 END schematic2;
 
 ARCHITECTURE behaviour OF schematic2 IS 
 BEGIN 
-
+    d <= ((not a) and c) or (not a and b) or (b and c);
+    e <= (not a and not b and c) or (not a and b and not c) or (a and not b and not c) or (a and b and c);
 END behaviour;
