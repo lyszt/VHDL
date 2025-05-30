6057d225a1e501379c77712aa48c62507e11fdea
lyszt <kalliddel@gmail.com>
2025-05-30 16:50:56 -0300

    Remove linha inutil que eu esqueci

--- blackjack.vhdl
+++ blackjack.vhdl
@@ -225,19 +225,6 @@ BEGIN
 
         END IF;
         
-        WITH D SELECT
-        S <= 
-            "0111111" WHEN "0000",  
-            "0000110" WHEN "0001",  
-            "1011011" WHEN "0010",  
-            "1001111" WHEN "0011",  
-            "1100110" WHEN "0100",  
-            "1101101" WHEN "0101",  
-            "1111101" WHEN "0110",  
-            "0000111" WHEN "0111",  
-            "1111111" WHEN "1000",  
-            "1101111" WHEN "1001",  
-            "0000000" WHEN OTHERS; 
 END PROCESS;
 END ARCHITECTURE behavioral;
 
